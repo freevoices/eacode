c2f131dc10e85b9cee15032c40227239c8499cf8
Neopatron Latam <hola@neopatron.com>
2023-08-22 15:26:34 -0500

    .

--- frontend/src/app/communities/SugerirNav.jsx
+++ frontend/src/app/communities/SugerirNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {FaPlus} from 'react-icons/fa6'
+import { FaPlus } from 'react-icons/fa6'
 
 function SugerirNav() {
     return (
@@ -8,10 +8,12 @@ function SugerirNav() {
             <div className="bg-white border-b border-gray-300 sticky top-16">
                 <div className="mx-auto max-w-screen-sm px-4 py-1">
 
-                    <button className="btn btn-ghost text-red-700">
-                        <FaPlus/>
-                        SUGERIR UNA COMUNIDAD
-                    </button>
+                    <a href='https://forms.gle/3f3Pas3vR2e77qFK7' target='_blank'>
+                        <button className="btn btn-ghost text-red-700">
+                            <FaPlus />
+                            SUGERIR UNA COMUNIDAD
+                        </button>
+                    </a>
 
                 </div>
             </div>
