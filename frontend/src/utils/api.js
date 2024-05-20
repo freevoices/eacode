d513d7d053efd62ebe9aef0dc63f1dd2497fd5e6
Neopatron Latam <hola@neopatron.com>
2024-05-19 22:20:05 -0500

    fine

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,7 @@
 // utils/api.js
 //const apiURL = "https://api.secretos.pro/api";
-const apiURL = "http://localhost:1337/api";
+
+const strapiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+const apiURL = `${strapiUrl}/api`;
 export default apiURL;
